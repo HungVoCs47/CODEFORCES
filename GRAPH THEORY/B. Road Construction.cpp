68f7f8ba521537a33cbe45df2be14b7d5701a773
Võ Duy Hùng <76021536+HungVoCs47@users.noreply.github.com>
2021-02-03 20:17:28 +0700

    Add files via upload

---
+++ GRAPH THEORY/B. Road Construction.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define int ll
+#define FIN ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    int arr[n+1]={0};
+    for(int i=0;i<m;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        arr[a]++;
+        arr[b]++;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(arr[i]==0)
+        {
+            cout<<n-1<<endl;
+            for(int j=1;j<=n;j++)
+            {
+                if(i==j)
+                continue;
+                cout<<i<<" "<<j<<endl;
+            }
+            return;
+        }
+    }
+}
+main() 
+{
+	FIN;
+	solve();  
+}
---
+++ GRAPH THEORY/E. Polygon.cpp
@@ -0,0 +1,43 @@
+# include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int testcases;
+	cin>>testcases;
+	while(testcases--)
+	{
+		int n;
+		cin>>n;
+		char a[55][55];
+		for(int i=0;i<n;i++)
+	    {
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		cin>>a[i][j];
+			}
+		}
+		bool ans=true;
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=0;j<n-1;j++)
+			{
+				if(a[i][j]=='1')
+				{
+					if(a[i+1][j]=='1' || a[i][j+1]=='1')
+					{
+						continue;
+					}
+					else
+					{
+						ans=false;
+						break;
+					}
+				}
+			}
+		}
+		if(ans) cout<<"Yes\n";
+		else cout<<"No\n";
+	}
+	return 0;
+}
