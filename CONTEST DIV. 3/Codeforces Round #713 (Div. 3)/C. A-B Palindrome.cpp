cfef83c15ccf738d7162fb49ef79f74493aeda0b
Võ Duy Hùng <76021536+HungVoCs47@users.noreply.github.com>
2021-04-15 10:44:43 +0700

    Update C. A-B Palindrome.cpp

--- CONTEST DIV. 3/Codeforces Round #713 (Div. 3)/C. A-B Palindrome.cpp
+++ CONTEST DIV. 3/Codeforces Round #713 (Div. 3)/C. A-B Palindrome.cpp
@@ -1,6 +1,20 @@
 # include <bits/stdc++.h>
 using namespace std;
-# define _io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef pair<int,int>ii;
+#define ll long long
+#define endl cout<<"\n"
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vp vector<ii>
+#define sort_increase(a,n) sort(a,a+n)
+#define sort_decrease(a,n) sort(a,a+n,greater<int>())
+#define all(a) a.begin(),a.end()
+#define _io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define loop0(n) for(int i=0;i<n;i++)
+#define loop1(n) for(int i=1;i<=n;i++)
+const int M=1e9+7;
+const int N=3e5+10;
 int main()
 {
 	_io;
